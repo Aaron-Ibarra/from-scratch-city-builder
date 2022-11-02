1783494a865b032f692e7813e181e4ef053ddad3
Beau Vandervort <beauvandervort55@gmail.com>
2022-11-02 16:58:12 -0700

    slice 1 html complete

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const heroEl = document.getElementById('hero');
 /* State */
 
 /* Events */
 
 /* Display Functions */
-
+heroEl.style.backgroundImage = `url('https://placekitten.com/200/300')`;
 // (don't forget to call any display functions you want to run on page load!)
