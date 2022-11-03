4af84a571c573951031470b41ab7826490767159
Aaron Ibarra <aaronibarra132@gmail.com>
2022-11-02 17:32:22 -0700

    finished villain select dropdown and tracker

--- app.js
+++ app.js
@@ -3,13 +3,16 @@
 /* Get DOM Elements */
 const heroEl = document.getElementById('hero');
 const arenaEl = document.getElementById('arena');
+const villainEl = document.getElementById('villain');
 
 const heroSelect = document.getElementById('hero-dropdown');
 const arenaSelect = document.getElementById('arena-dropdown');
+const villainSelect = document.getElementById('villain-dropdown');
 
 /* State */
 let heroCounter = 0;
 let arenaCounter = 0;
+let villainCounter = 0;
 
 /* Events */
 heroSelect.addEventListener('change', (e) => {
@@ -22,6 +25,14 @@ arenaSelect.addEventListener('change', (e) => {
     const value = e.target.value;
     arenaCounter++;
     //arenaEl.style.backgroundImage = `url('')`;
+});
+
+villainSelect.addEventListener('change', (e) => {
+    const value = e.target.value;
+    villainCounter++;
+    //villainEl.style.backgroundImage = `url('')`;
+    console.log(value);
+    console.log(villainCounter);
 });
 
 /* Display Functions */
