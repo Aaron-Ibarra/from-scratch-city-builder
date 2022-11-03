50ae61e39a5d880941d0c1bcb020ec3375a5b179
Austin Papritz <austin@papritz.dev>
2022-11-02 17:22:41 -0700

    arena HTML, arena DOM elements, arena eventlistener with incrementing arenaCounter

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 
 /* Get DOM Elements */
 const heroEl = document.getElementById('hero');
+const arenaEl = document.getElementById('arena');
+
 const heroSelect = document.getElementById('hero-dropdown');
+const arenaSelect = document.getElementById('arena-dropdown');
+
 /* State */
 let heroCounter = 0;
+let arenaCounter = 0;
+
 /* Events */
 heroSelect.addEventListener('change', (e) => {
     const value = e.target.value;
@@ -12,5 +18,12 @@ heroSelect.addEventListener('change', (e) => {
     //heroEl.style.backgroundImage = `url('')`;
     console.log(heroCounter);
 });
+
+arenaSelect.addEventListener('change', (e) => {
+    const value = e.target.value;
+    arenaCounter++;
+    console.log(arenaCounter);
+});
+
 /* Display Functions */
 // (don't forget to call any display functions you want to run on page load!)
